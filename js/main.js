f3759a25a1ef119bf02c36b343f82344e7da4697
Kristen Quinlan-Boyce <kqboyce@gmail.com>
2019-04-04 10:44:20 -0400

    added autocomplete and other functionality

--- js/main.js
+++ js/main.js
@@ -1,41 +1,202 @@
+// message user widget
+  // search user autocomplete
+const newMembers = [
+    'Victoria Chambers',
+    'Dale Byrd',
+    'Dawn Wood',
+    'Dan Oliver',
+    'Jane Sullivan'
+  ];
+  const searchUser = document.getElementById('searchUser');
+  const userName = searchUser.value;
+  function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
+}
 
+autocomplete(searchUser, newMembers);
 
-// searchForm.addEventListener("keyup", function() {
-//     // var inputWord = keyword.value.toLowerCase();
-//     for (i=0; i < 12; i++) {
-//       var imageCapt = document.getElementsByTagName("A")[i].getAttribute("data-title").toLowerCase();
-//       if (imageCapt.indexOf(keyword.value.toLowerCase()) > -1) {
-//         images[i].style.display = '';
-//       } else {
-//         images[i].style.display = 'none';
-//         }
-//     }
+  //
+  // const searchUser = document.getElementById('searchUser');
+  // const userName = searchUser.value;
+  // searchUser.addEventListener("keyup", function() {
+  //     for (i=0; i < newMembers.length; i++) {
+  //       let memberLetts = Array.from(newMembers[i].toLowerCase());
+  //
+  //       if (memberLetts.indexOf(userName.toLowerCase()) > -1) {
+  //         userName.value = memberList.value;
+  //         // members[i].style.display = '';
+  //       }
+  //       // else {
+  //       //   newMembers[i].style.display = 'none';
+  //       //   }
+  //     }
+  // });
+//
+//   document.addEventListener('keyup', function() {
+//     const searchMembers = document.getElementById('searchMembers');
+//     const searchValue =  searchMembers.value.toUpperCase();
+//       console.log(searchValue);
 // });
-
-
-// alerts drop down menu
-const iconAlert = document.getElementById('iconAlert');
-const ul = document.createElement('ul');
-const alertDiv = document.createElement('div');
-iconAlert.addEventListener('click', () => {
-  const user = document.getElementById('user');
-
-  user.appendChild(ul);
-  user.classList.toggle('show');
-  const li = document.createElement('li');
-  li.textContent="lorem ipsum";
-  ul.appendChild(li);
-
+//   let nameArray = members[i].split('');
+//   for (let i =0; i <members.length; i++) {
+//
+//   const searchMembers = document.getElementById('searchMembers');
+//   document.addEventListener('keyup', function() {
+//     const searchValue =  searchMembers.value.toUpperCase();
+//     if(nameArray.toUpperCase == searchValue) {
+//       console.log('searchMembers');
+//     }
+//
+//
+//   });
+// }
+const message = document.getElementById('message');
+const sendMessage = document.getElementById('sendMessage');
+sendMessage.addEventListener('click', function(e) {
+  e.preventDefault();
+  if (searchUser.value == '') {
+    window.alert('You must search for and select a member.');
+    } else if(message.value == '') {
+      window.alert('Please enter a message for ' + searchUser.value + '!');
+    } else {
+    window.alert('Your message to ' + searchUser.value + ' was sent!');
+    }
 });
 
-// add active class to selected items
-//
-// const addActive = (e)=> {
-//     e.target.classList.toggle('active');
-// }
-// const iconNav = document.getElementById('icon-menu');
-// iconNav.addEventListener('click', addActive);
 
+// alerts and drop down menu
+  // trigger bell animation when mouse hovers over badge
+const alertBadge = document.getElementById('iconAlert');
+const bell = document.getElementById('bellIcon');
+alertBadge.addEventListener('mouseover', () => {
+    bell.classList.add('bellAnim');
+  alertBadge.addEventListener('mouseout', () => {
+    bell.classList.remove('bellAnim');
+  });
+});
+
+  // reveal recent alerts window
+const alertWindow = document.getElementById('alertWindow');
+const iconAlert = document.getElementById('iconAlert');
+iconAlert.addEventListener('click', () => {
+  alertWindow.classList.remove('hide');
+  alertWindow.classList.add('embed');
+});
+
+//close alerts in pop-up window
+const closebtns = document.getElementsByClassName("close");
+for (let i = 0; i < closebtns.length; i++) {
+  closebtns[i].addEventListener("click", function() {
+  this.parentElement.style.display = 'none';
+  for(let i=3; i>closebtns.length; i--) {
+    closebtns[i].addEventListener('click', function() {
+      iconAlert.innerHTML = (4-i);
+    });
+    }
+  });
+}
+
+// close Alert box
+const alert = document.getElementById('alert');
+const hideButton = document.getElementById('hideButton');
+hideButton.addEventListener('click', function() {
+  alert.classList.add('hide');
+});
+
+// add active class to icon menu when icon clicked
 const iconMenu = document.getElementById("nav");
 var icons = document.getElementsByClassName("icons");
 for (var i = 0; i < icons.length; i++) {
@@ -100,8 +261,6 @@ const saveSettings = document.getElementById('saveSettings');
    saveSet();
  });
 
-
-
 window.onload = function() {
   let publicProf = document.getElementById('publicProf');
   let emailNotify = document.getElementById('emailNotify');
@@ -115,55 +274,8 @@ const remember = () => {
   remember();
 }
 
-    document.getElementById('sendMessage').addEventListener('click', function(event) {
-      event.preventDefault();
-      const message = document.getElementById('message').value;
-      const searchUser = document.getElementById('searchUser').value;
-
-      localStorage.setItem('message', message);
-      localStorage.setItem('searchUser', searchUser);
-
-      window.alert('what?')
-    });
-  }
-
-const alert = document.getElementById('alert');
-const hideButton = document.getElementById('hideButton');
-hideButton.addEventListener('click', function() {
-  alert.classList.add('hide');
-});
-
-const members = [
-    'Victoria Chambers',
-    'Dale Byrd',
-    'Dawn Wood',
-    'Dan Oliver',
-    'Jane Sullivan'
-  ];
-  document.addEventListener('keyup', function() {
-    const searchMembers = document.getElementById('searchMembers');
-    const searchValue =  searchMembers.value.toUpperCase();
-      console.log(searchValue);
-});
-//   let nameArray = members[i].split('');
-//   for (let i =0; i <members.length; i++) {
-//
-//   const searchMembers = document.getElementById('searchMembers');
-//   document.addEventListener('keyup', function() {
-//     const searchValue =  searchMembers.value.toUpperCase();
-//     if(nameArray.toUpperCase == searchValue) {
-//       console.log('searchMembers');
-//     }
-//
-//
-//   });
-// }
-
-
-
 // CHARTS
 // Line charts
-
 
 // var ctx = document.getElementById('lineChart').getContext('2d');
 // const barData = {
@@ -319,3 +431,4 @@ var myDoughnutChart = new Chart(ctx, {
     },
     options: {}
 });
+}
